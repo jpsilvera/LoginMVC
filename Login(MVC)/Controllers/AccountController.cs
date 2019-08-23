854aa5119f4df5d4cdab8161ecafc2427932f376
Juan Pablo Silvera <juan.silvera@bp-4.com>
2019-08-23 17:06:47 -0300

    Add project files.

---
+++ Login(MVC)/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login_MVC_.Models;
+using System.Data.SqlClient;
+
+namespace Login_MVC_.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = "data source=SQLEXPRESS; database=WPF; integrated security=SSPI;";
+        }
+
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            
+            con.Close();
+            return View();
+        }
+    }
+}
